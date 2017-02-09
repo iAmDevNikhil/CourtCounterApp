3f44e550d0c897ed10bcd76bd3836321fdccd807
nikhildevaraj <nikhildevaraj1991@gmail.com>
2017-02-09 15:39:57 -0500

    Created UI for side TeamA with minimal functionality.

---
+++ app/src/main/java/com/example/nikky/courtcounter/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.nikky.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOneForTeamA(View view) {
+        displayForTeamA(1);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View view){
+        displayForTeamA(2);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View view){
+        displayForTeamA(3);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
